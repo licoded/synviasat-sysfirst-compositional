8f66542bf4f867cb9d116cc09f97c83bc47a12ae
licoded <busy.li@foxmail.com>
2024-06-10 17:18:54 +0800

    etc/scanners: opt to make it could handle more nested folder files

--- etc/scanners.cmake
+++ etc/scanners.cmake
@@ -7,12 +7,11 @@ file (GLOB_RECURSE LTLFSYN_CXX_FILES
 add_custom_target (format "clang-format" -i ${LTLFSYN_CXX_FILES} COMMENT "Formatting source code...")
 
 foreach (tidy_target ${LTLFSYN_CXX_FILES})
-  get_filename_component (basename ${tidy_target} NAME)
-  get_filename_component (dirname ${tidy_target} DIRECTORY)
-  get_filename_component (basedir ${dirname} NAME)
-  set (tidy_target_name "${basedir}__${basename}")
+  string(REPLACE "${CMAKE_SOURCE_DIR}/" "" relative_path ${tidy_target})
+  string(REPLACE "/" "__" tidy_target_name "${relative_path}")
   set (tidy_command clang-tidy --quiet -header-filter=.* -p=${PROJECT_BINARY_DIR} ${tidy_target})
   add_custom_target (tidy_${tidy_target_name} ${tidy_command})
+  # message(STATUS "tidy_target_name: ${tidy_target_name}")
   list (APPEND LTLFSYN_TIDY_TARGETS tidy_${tidy_target_name})
 endforeach (tidy_target)
 
