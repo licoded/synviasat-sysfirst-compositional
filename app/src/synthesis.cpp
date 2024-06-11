d5358b832abb748a9f115be5dcd337e657615a8f
licoded <busy.li@foxmail.com>
2024-06-11 18:08:26 +0800

    app: split af by and

--- app/src/synthesis.cpp
+++ app/src/synthesis.cpp
@@ -16,5 +16,16 @@ bool is_realizable(aalta::aalta_formula *src_formula, std::unordered_set<std::st
     syn_states::insert_ewin_state(FormulaInBdd::FALSE_bddP_);
     Syn_Frame *init = new Syn_Frame(src_formula);
 
-    return forwardSearch(init);
+    hash_set<aalta_formula *> and_sub_afs = src_formula->and_to_set();
+    for (auto it : and_sub_afs)
+    {
+        Syn_Frame *init = new Syn_Frame(it);
+        if (!forwardSearch(init))
+            return false;
+    }
+
+    if (and_sub_afs.size() == 1)
+        return true;
+
+    return true;
 }
