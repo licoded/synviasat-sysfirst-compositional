48376b1be32d186bc57148fb517ac57a8f295835
licoded <busy.li@foxmail.com>
2024-06-11 11:49:18 +0800

    cmake: all OK: libdeps libltlfsat libltlfsyn

---
+++ lib/include/ltlfsyn/synthesis.h
@@ -0,0 +1,65 @@
+#ifndef __SYNTHESIS__
+#define __SYNTHESIS__
+
+#include <cassert>
+#include <unordered_set>
+#include <set>
+
+#include "formula/aalta_formula.h"
+#include "ltlfsyn/edge_cons.h"
+#include "synutil/formula_in_bdd.h"
+#include "synutil/syn_type.h"
+#include "cudd/cudd.h"
+
+using namespace std;
+using namespace aalta;
+
+extern bool SAT_TRACE_FLAG;
+
+typedef unsigned long long ull;
+
+// main entry
+bool is_realizable(aalta_formula *src_formula, unordered_set<string> &env_var, bool verbose);
+
+class edgeCons;
+
+class Syn_Frame
+{
+private:
+    FormulaInBdd *state_in_bdd_;
+    edgeCons *edgeCons_;
+    Status status_;
+
+    int swin_checked_idx_;
+    int ewin_checked_idx_;
+
+public:
+
+    Syn_Frame(aalta_formula *af);
+    ~Syn_Frame();
+
+    DdNode *GetBddPointer() { return state_in_bdd_->GetBddPointer(); }
+    aalta_formula *GetFormulaPointer() { return state_in_bdd_->GetFormulaPointer(); }
+
+    Status checkStatus();
+    void processSignal(Signal sig, DdNode *succ);
+
+    bool getEdge(unordered_set<int> &edge, queue<pair<aalta_formula *, aalta_formula *>> &model);
+    Status get_status() { return status_; }
+
+    bool checkSwinForBackwardSearch();
+};
+
+bool forwardSearch(Syn_Frame *);
+void backwardSearch(std::vector<Syn_Frame *> &scc);
+
+// for tarjan
+void initial_tarjan_frame(Syn_Frame *cur_frame);
+void update_by_low(Syn_Frame *cur_frame, DdNode *cur_bddP);
+void update_by_dfn(Syn_Frame *cur_frame, Syn_Frame *next_frame);
+void getScc(int cur, std::vector<Syn_Frame *> &scc,
+            vector<Syn_Frame *> &sta, unordered_map<ull, int> &sta_bdd2curIdx_map);
+
+void PartitionAtoms(aalta_formula *af, unordered_set<string> &env_val);
+
+#endif
---
+++ lib/include/synutil/syn_states.cpp
@@ -0,0 +1,51 @@
+#include "synutil/syn_states.h"
+
+
+std::unordered_set<ull> syn_states::swin_state_bdd_set;
+std::unordered_set<ull> syn_states::ewin_state_bdd_set;
+std::unordered_set<ull> syn_states::dfs_complete_state_bdd_set;
+std::vector<DdNode *> syn_states::swin_state_bdd_vec;
+std::vector<DdNode *> syn_states::ewin_state_bdd_vec;
+std::unordered_map<ull, set<DdNode *> *> syn_states::predecessors;
+
+Status syn_states::getBddStatus(DdNode *b)
+{
+    if (swin_state_bdd_set.find(ull(b)) != swin_state_bdd_set.end())
+        return Swin;
+    else if (ewin_state_bdd_set.find(ull(b)) != ewin_state_bdd_set.end())
+        return Ewin;
+    else if (dfs_complete_state_bdd_set.find(ull(b)) != dfs_complete_state_bdd_set.end())
+        return Dfs_complete;
+    else
+        return Dfs_incomplete;
+}
+
+Status syn_states::get_status_by_set(DdNode *bddp)
+{
+    if (swin_state_bdd_set.find(ull(bddp)) != swin_state_bdd_set.end())
+        return Swin;
+    if (ewin_state_bdd_set.find(ull(bddp)) != ewin_state_bdd_set.end())
+        return Ewin;
+    if (dfs_complete_state_bdd_set.find(ull(bddp)) != dfs_complete_state_bdd_set.end())
+        return Dfs_complete;
+    return Dfs_incomplete;
+}
+
+Status syn_states::get_status_by_set_imply(DdNode *bddp, int swin_checked_idx, int ewin_checked_idx)
+{
+    int vec_size = swin_state_bdd_vec.size();
+    for (; swin_checked_idx < vec_size; ++swin_checked_idx)
+        if (FormulaInBdd::Implies(swin_state_bdd_vec[swin_checked_idx], bddp))
+        {
+            syn_states::insert_swin_state(bddp, true);
+            return Swin;
+        }
+    vec_size = ewin_state_bdd_vec.size();
+    for (; ewin_checked_idx < vec_size; ++ewin_checked_idx)
+        if (FormulaInBdd::Implies(bddp, ewin_state_bdd_vec[ewin_checked_idx]))
+        {
+            syn_states::insert_ewin_state(bddp, true);
+            return Ewin;
+        }
+    return Dfs_incomplete;
+}
