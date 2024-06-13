521ab442e4657a74455ac5b62a5ed84807ef30da
licoded <busy.li@foxmail.com>
2024-06-13 23:39:42 +0800

    etc/scanners: specified config file for clang-tidy
    TODO: I havn't check whether the .clang-tidy config file in rootpath
        - is defaultly being used/chosen
        - could work

--- etc/scanners.cmake
+++ etc/scanners.cmake
@@ -7,12 +7,21 @@ file (GLOB_RECURSE LTLFSYN_CXX_FILES
 file (GLOB_RECURSE LTLFSAT_CXX_FILES ${LIBRARY_INCLUDE_PATH}/ltlfsat/*)
 list(REMOVE_ITEM LTLFSYN_CXX_FILES ${LTLFSAT_CXX_FILES})
 
+# function(print_list list_var)
+#     message(STATUS "List of files:")
+#     foreach(item ${${list_var}})
+#         message(STATUS "  ${item}")
+#     endforeach()
+# endfunction()
+
+# print_list(LTLFSYN_CXX_FILES)
+
 add_custom_target (format "clang-format" -i ${LTLFSYN_CXX_FILES} COMMENT "Formatting source code...")
 
 foreach (tidy_target ${LTLFSYN_CXX_FILES})
   string(REPLACE "${CMAKE_SOURCE_DIR}/" "" relative_path ${tidy_target})
   string(REPLACE "/" "__" tidy_target_name "${relative_path}")
-  set (tidy_command clang-tidy --quiet -header-filter=.* -p=${PROJECT_BINARY_DIR} ${tidy_target})
+  set (tidy_command clang-tidy --quiet -header-filter=.* --config-file=${CMAKE_SOURCE_DIR}/.clang-tidy -p=${PROJECT_BINARY_DIR} ${tidy_target})
   add_custom_target (tidy_${tidy_target_name} ${tidy_command})
   # message(STATUS "tidy_target_name: ${tidy_target_name}")
   list (APPEND LTLFSYN_TIDY_TARGETS tidy_${tidy_target_name})
